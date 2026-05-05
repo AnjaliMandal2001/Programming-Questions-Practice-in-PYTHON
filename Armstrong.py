5f2e8a264e24c38aac732fa88a3d4f9ff6deffd6
Anjali Mandal <mandalanjali2001@gmail.com>
2026-05-05 22:25:24 +0530

    Intial

--- Armstrong.py
+++ Armstrong.py
@@ -1,3 +1,35 @@
+'''
+An Armstrong number is a number equal to the sum of its digits raised to the power of the number of digits.
+🔍 Example: 153
+
+Number of digits = 3
+
+Calculation:
+
+(1)3=1
+(5)3=125
+(3)3=27
+
+Sum = 1 + 125 + 27 = 153 ✅
+
+So, 153 is an Armstrong number
+
+🔍 Another Example: 9474
+
+Digits = 4
+
+(9)4=6561
+(4)4=256
+(7)4=2401
+(4)4=256
+
+Sum = 6561 + 256 + 2401 + 256 = 9474 ✅
+
+So, 9474 is also an Armstrong number
+'''
+
+
+
 num=int(input("Enter any number:"))
 org=num
 sum=0
---
+++ Perfect.py
@@ -0,0 +1,20 @@
+'''
+A perfect number is a number that is equal to the sum of its proper divisors (excluding the number itself).
+
+📌 Example: 6
+
+Divisors of 6 → 1, 2, 3, 6
+Proper divisors → 1, 2, 3
+
+Sum = 1 + 2 + 3 = 6 ✅
+So, 6 is a perfect number
+
+📌 Another Example: 28
+
+Divisors → 1, 2, 4, 7, 14, 28
+Proper divisors → 1, 2, 4, 7, 14
+
+Sum = 1 + 2 + 4 + 7 + 14 = 28 ✅
+So, 28 is also a perfect number
+'''
+
