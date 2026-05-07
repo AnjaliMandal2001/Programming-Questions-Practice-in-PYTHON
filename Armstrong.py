c86040e9b9217528b766a07863d6ce72e6e843cf
Anjali Mandal <mandalanjali2001@gmail.com>
2026-05-07 10:04:54 +0530

    New code

--- Armstrong.py
+++ Armstrong.py
@@ -29,17 +29,46 @@ So, 9474 is also an Armstrong number
 '''
 
 
+# Method 1
 
+''' 
 num=int(input("Enter any number:"))
+digit=int(input("Enter number of digit like- 3 or 4: "))
+
 org=num
 sum=0
+if digit==3:
+   while num>0:
+     sum=sum+ (num%10)*(num%10)*(num%10)
+     num=num//10
 
-while num>0:
-    sum=sum+ (num%10)*(num%10)*(num%10)
-    num=num//10
+else:
+   while num>0:
+     sum=sum+ (num%10)*(num%10)*(num%10)*(num%10)
+     num=num//10
+
 
 if sum==org:
     print(f"{org} is a Armstrong number.")
 
 else:
     print("It's not a Armstrong number.")
+'''
+
+
+# Method 2
+num=int(input("Enter any number:"))
+digit=int(input("Enter number of digit like- 3 or 4: "))
+
+org=num
+sum=0
+
+while num>0:
+    rem=num%10
+    sum=sum+rem**digit
+    num=num//10
+
+if sum == org:
+    print(f"{org} is an Armstrong number.")
+else:
+    print(f"{org} is not an Armstrong number.")
